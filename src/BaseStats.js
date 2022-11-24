209592212d16d00aa8a5d324f658ae45a69ae532
Samuel Park <samjpark1497@gmail.com>
2022-11-24 01:53:45 +0000

    Update App.js and BaseStats.js

--- src/BaseStats.js
+++ src/BaseStats.js
@@ -1,8 +1,31 @@
-import './BaseStats.css'
-export default function BaseStats(props) {
-    return (
-        <div className='base-stats'>
-            <h1>BaseStats</h1>
-        </div>
-    );
+import "./BaseStats.css";
+export default function BaseStats({ stats: { hp, attack, defense, speed }, clicker }) {
+  return (
+    <div className="base-stats">
+      <h1>BaseStats</h1>
+      <button className="sp-stats" onClick={clicker} type="button">
+          Check Special Stats
+      </button>
+      <table>
+        <tbody>
+          <tr>
+            <td>Hit Points</td>
+            <td>{hp}</td>
+          </tr>
+          <tr>
+            <td>Attack</td>
+            <td>{attack}</td>
+          </tr>
+          <tr>
+            <td>Defense</td>
+            <td>{defense}</td>
+          </tr>
+          <tr>
+            <td>Speed</td>
+            <td>{speed}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
